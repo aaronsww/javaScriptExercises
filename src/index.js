7c9db8ba085a3f068813835ea8e52d11d9213fd2
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-01-13 14:19:10 +0530

    style: basic project layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("say hi twice")
+function generate(title, description, dueDate,){
+    return {
+        title,
+        description, 
+        dueDate,
+    }
+}
+
+// for(let i = 0; i < 10; i++)
+// {
+//     const a[i] =  generate("first toDo", "I really need this", "2022-03-25");
+// }
+
+// console.log(a[3].title)
